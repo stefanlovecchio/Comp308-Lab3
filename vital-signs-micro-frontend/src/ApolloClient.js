012e34220b65b735704438fa4f7a6f06263bfeec
Stefan Lovecchio <slovecch@my.centennialcollege.ca>
2024-11-17 16:37:52 -0500

    connected VitalSigns microservice & microfrontend. deleted gateway, installed node modules.

--- vital-signs-micro-frontend/src/ApolloClient.js
+++ vital-signs-micro-frontend/src/ApolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+  uri: 'http://localhost:3002/graphql',
+  cache: new InMemoryCache(),
+  credentials: 'include',
 });
 
 export default client;
